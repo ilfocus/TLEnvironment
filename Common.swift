9e02cdda845dad6717126d7581e8162508df88de
ilfocus <340690100@qq.com>
2024-10-20 21:31:03 +0800

    feat: add some common data

---
+++ Common.swift
@@ -0,0 +1,8 @@
+//
+//  Common.swift
+//  Pods-TLEnvironment_Example
+//
+//  Created by qi wang on 2024/10/20.
+//
+
+import Foundation
---
+++ Environment.swift
@@ -0,0 +1,8 @@
+//
+//  Environment.swift
+//  Pods-TLEnvironment_Example
+//
+//  Created by qi wang on 2024/10/20.
+//
+
+import Foundation
